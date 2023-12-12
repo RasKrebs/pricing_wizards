6e334d0e0addf295adef815ed6d7a9e516c7824c
Rasmus Krebs <rasmuskrebs@Rasmuss-MBP-2.localdomain>
2023-12-12 15:07:28 +0100

    Added linear regression model trained. Updated dataloader for use in other scripts. Created train notebook and python file that can be executed along with given arguments to train certain models. Additionally added some helper files to save models, store datatransformations (for clean code in train files) etc.

---
+++ models/base_linear_regression.py
@@ -0,0 +1,37 @@
+from utils.RegressionEvaluation import regression_accuracy
+from sklearn.linear_model import LinearRegression
+
+# Model
+def linear_regression(dataset):
+    """
+    Run a machine learning model using hyperparameter tuning with both GridSearchCV and RandomizedSearchCV.
+
+    Parameters:
+    - dataset (object): An object containing data and configurations for model training and testing.
+
+    Returns:
+    - results (dict): A dictionary containing the regression results and model
+    """
+
+    # Create a Linear Regression model
+    lr = LinearRegression()
+
+    # Fit the model to the training data
+    lr.fit(dataset.X_train, dataset.y_train)
+
+    # Make predictions on the test data
+    y_pred = lr.predict(dataset.X_test)
+
+    # Calculate metrics
+    r2, mse, mae, rmse = regression_accuracy(y_pred, dataset.y_test, return_metrics=True)
+
+    # Create a dictionary to store results and model
+    results = {
+        'model': lr,
+        'r2': r2,
+        'mse': mse,
+        'mae': mae,
+        'rmse': rmse,
+    }
+
+    return results
