d638d27cd35c0d36bec283d552e493e51216c4ad
Rasmus Krebs <rasmuskrebs@Rasmuss-MacBook-Pro-2.local>
2023-12-16 17:20:29 +0100

    Added neural network regression model as well as other small changes

--- utils/DataTransformation.py
+++ utils/DataTransformation.py
@@ -1,39 +1,70 @@
 import pandas as pd
 import numpy as np
 
+def condition_encoding(df):
+    """
+    Ordinal encoding for condition
+    """
+    # Condition name ordered
+    condition_name = ['Shabby', 'Good but used','Almost as new', 'Never used', 'New, still with price']
+    
+    # Apply ordinal encoding
+    df.condition_name = df.condition_name.apply(lambda x: condition_name.index(x) +1)   
+    
+    return df
+
+
+def price_encoding(df, column, listing_price_column='log_listing_price'):
+    """
+    Ordinal encoding for brand
+    """
+    brand_encoding = (df
+                      .groupby(column)
+                      .agg({listing_price_column: 'mean'})
+                      .sort_values(by=listing_price_column, ascending=True)
+                      .to_dict()[listing_price_column])
+    
+    df[column] = df[column].apply(lambda x: brand_encoding[x])
+    
+    return df
+
+def filter_rare_categories(df, col, replacement, minimum=30):
+    """
+    Filter rare subsubsub categories
+    """
+    # Set minimum
+    minimum = minimum
+    
+    # Extract rare categories
+    rare_categories = pd.DataFrame(df[col].value_counts()).where(df[col].value_counts() < minimum).dropna().index
+    
+    # Replace
+    df.loc[df[df[col].isin(rare_categories)].index, 'subsubsubcategory_name'] = df[df[col].isin(rare_categories)][replacement]
+    
+    # Few subsubsubsub categories remain - these are mapped to subcategory instead
+    maps = {'Sports shoes': 'Shoes', # Less granular
+            'Sportsudstyr': 'Sport', # Less granular
+            'Smartphones & Accessories': 'Accessories'} 
+    
+    # Replacing the few remaining rare cases
+    df.loc[df[df.subsubsubcategory_name.isin(maps.keys())].index, 'subsubsubcategory_name'] = df[df.subsubsubcategory_name.isin(maps.keys())].subsubsubcategory_name.map(maps)
+    
+    return df
+
+
+
 # Data Preperation for Regression
 def base_regression_pipeline(df):
     """
     Prepare data for regression
     """
-    
-    # Ordinal Encoding for condition, since this typically follows some sort of order
-    condition_name = ['Shabby', 'Good but used','Almost as new', 'Never used', 'New, still with price']
 
     # Replacing rare subsubsub categories with subsubcategories
-    minimum = 30 
-    rare_sub_categories = pd.DataFrame(df.subsubsubcategory_name.value_counts()).where(df.subsubsubcategory_name.value_counts() < minimum).dropna().index
-    df.loc[df[df.subsubsubcategory_name.isin(rare_sub_categories)].index, 'subsubsubcategory_name'] = df[df.subsubsubcategory_name.isin(rare_sub_categories)].subcategory_name
+    df = filter_rare_categories(df, 'subsubsubcategory_name', 'subsubcategory_name')
+    df = condition_encoding(df)
+    df = price_encoding(df, 'brand_name')
+    df = price_encoding(df, 'subsubsubcategory_name')
     
-    # Mapping brand and subsubsubsub categories average listing prices
-    brand_encoding = (df
-                      .groupby('brand_name')
-                      .agg({'log_listing_price': 'mean'})
-                      .sort_values(by='log_listing_price', ascending=True)
-                      .to_dict()['log_listing_price'])
-    
-    subsubsubcategory_encoding = (df
-                                  .groupby('subsubsubcategory_name')
-                                  .agg({'log_listing_price': 'mean'})
-                                  .sort_values(by='log_listing_price', ascending=True)
-                                  .to_dict()['log_listing_price'])
-    
-    # Apply transformations
-    df = df.assign(
-        condition_name = lambda x: x['condition_name'].apply(lambda x: condition_name.index(x) +1),
-        brand_name = lambda x: x['brand_name'].apply(lambda x: brand_encoding[x]),
-        subsubsubcategory_name = lambda x: x['subsubsubcategory_name'].apply(lambda x: subsubsubcategory_encoding[x])
-    )
     # Final columns to use
     columns_to_use = ['classified_id','log_listing_price','brand_name','condition_name','subsubsubcategory_name']
     df = df[columns_to_use]
@@ -42,27 +73,14 @@ def base_regression_pipeline(df):
     return df
 
 def ridge_regression_pipeline(df):
+    
     # Data transformation for ridge regression
     
-    # Minimum count for subsubsubsub category
-    minimum = 30
-    
-    # Mapping rare subsubsubsub categories to subcategory instead
-    rare_sub_categories = pd.DataFrame(df.subsubsubcategory_name.value_counts()).where(df.subsubsubcategory_name.value_counts() < minimum).dropna().index
-    
-    # Replacing with subcategory name
-    df.loc[df[df.subsubsubcategory_name.isin(rare_sub_categories)].index, 'subsubsubcategory_name'] = df[df.subsubsubcategory_name.isin(rare_sub_categories)].subsubcategory_name
-    
-    # Few subsubsubsub categories are still left
-    maps = {'Sports shoes': 'Shoes', # Less granular
-            'Sportsudstyr': 'Sport', # Less granular
-            'Smartphones & Accessories': 'Accessories'} 
-    
-    # Replacing the few remaining rare cases
-    df.loc[df[df.subsubsubcategory_name.isin(maps.keys())].index, 'subsubsubcategory_name'] = df[df.subsubsubcategory_name.isin(maps.keys())].subsubsubcategory_name.map(maps)
+    df = filter_rare_categories(df, 'subsubsubcategory_name', 'subsubcategory_name')
+    df = condition_encoding(df)
     
     # New columns to use
-    columns_to_use = ['classified_id', 'log_listing_price', 'brand_name','subsubsubcategory_name']
+    columns_to_use = ['classified_id', 'log_listing_price', 'condition_name','brand_name','subsubsubcategory_name']
 
     # Drop unused columns
     df = df[columns_to_use]
