3ae8f9a9047df2821742eaf70018d6454300b45b
Alberto Beyersdorff <albeertobfilho@gmail.com>
2023-12-17 20:16:18 +0100

    New tests

--- utils/helpers.py
+++ utils/helpers.py
@@ -62,7 +62,6 @@ def two_step_hyperparameter_tuning(model: Type[BaseEstimator], dataset: PricingW
         model,
         param_distributions=param_dist,
         scoring='neg_mean_squared_error',
-        cv=5,
         random_state=42,
         n_jobs=-1,
         verbose=3
